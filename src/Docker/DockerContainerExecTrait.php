4ead824ad2dede82b6a392bb9ca00ad2a57d4e4c
Jacob Sanford <jsanford@unb.ca>
2023-03-19 15:38:51 -0300

    Move container exec-ing to a trait

---
+++ src/Docker/DockerContainerExecTrait.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Dockworker\Docker;
+
+use Dockworker\Cli\DockerCliTrait;
+use Dockworker\Cli\KubectlCliTrait;
+use Dockworker\IO\DockworkerIO;
+use Dockworker\K8s\DeployedK8sResourcesTrait;
+use Robo\Robo;
+
+/**
+ * Provides methods for executing commands inside deployed docker containers.
+ */
+trait DockerContainerExecTrait
+{
+    use DeployedK8sResourcesTrait;
+    use DeployedLocalResourcesTrait;
+    use DockerCliTrait;
+    use KubectlCliTrait;
+
+    /**
+     * Executes a command in the deployed container.
+     *
+     * @param string $env
+     *   The environment to execute the command in.
+     * @param array $command
+     *   The command to execute.
+     * @param \Dockworker\IO\DockworkerIO $io
+     *   The IO to use for input and output.
+     * @param string $title
+     *   The title to display.
+     * @param string $message
+     *   The message to display.
+     * @param bool $init
+     *   Whether to initialize the command.
+     */
+    protected function executeContainerCommand(
+        string $env,
+        array $command,
+        DockworkerIO $io,
+        string $title = '',
+        string $message = '',
+        bool $init = true
+    ): void {
+        if ($init) {
+            $this->initContainerExecCommand($io, $env);
+        }
+        $container = $this->getDeployedContainer(
+            $io,
+            $env
+        );
+        if (!empty($title)) {
+            $io->title($title);
+        }
+        if (!empty($message)) {
+            $io->say($message);
+        }
+        $container->run(
+            $command,
+            $io
+        );
+    }
+
+    /**
+     * Executes a set of shell commands in the deployed container.
+     *
+     * @param string $env
+     *   The environment to execute the command in.
+     * @param array $commands
+     *   The commands to execute.
+     * @param \Dockworker\IO\DockworkerIO $io
+     *   The IO to use for input and output.
+     * @param string $title
+     *   The title to display.
+     */
+    protected function executeContainerCommandSet(
+        string $env,
+        array $commands,
+        DockworkerIO $io,
+        string $title = '',
+    ): void {
+        $first_command = true;
+        foreach ($commands as $command) {
+            if ($first_command) {
+                $title_string = $title;
+                $needs_init = true;
+                $first_command = false;
+            }
+            else {
+                $title_string = '';
+                $needs_init = false;
+            }
+            $this->executeContainerCommand(
+                $env,
+                $command['command'],
+                $io,
+                $title_string,
+                $command['message'],
+                $needs_init
+            );
+        }
+    }
+
+    /**
+     * Initializes the command and executes all preflight checks.
+     *
+     * @param \Dockworker\IO\DockworkerIO $io
+     *   The IO to use for input and output.
+     * @param string $env
+     *   The environment to initialize the command for.
+     */
+    protected function initContainerExecCommand(
+        DockworkerIO $io,
+        string $env
+    ): void
+    {
+        if ($env === 'local') {
+            $this->registerDockerCliTool($io);
+            $this->enableLocalResourceDiscovery();
+        } else {
+            $this->registerKubectlCliTool($io);
+            $this->enableK8sResourceDiscovery();
+        }
+        $this->checkPreflightChecks($io);
+        $this->discoverDeployedResources(
+            $io,
+            Robo::config(),
+            $env
+        );
+    }
+}
--- src/Robo/Plugin/Commands/DockworkerShellCommands.php
+++ src/Robo/Plugin/Commands/DockworkerShellCommands.php
@@ -2,12 +2,8 @@
 
 namespace Dockworker\Robo\Plugin\Commands;
 
-use Dockworker\Cli\DockerCliTrait;
-use Dockworker\Cli\KubectlCliTrait;
-use Dockworker\Docker\DeployedLocalResourcesTrait;
+use Dockworker\Docker\DockerContainerExecTrait;
 use Dockworker\DockworkerCommands;
-use Dockworker\GitHub\GitHubClientTrait;
-use Dockworker\K8s\DeployedK8sResourcesTrait;
 use Robo\Robo;
 
 /**
@@ -15,11 +11,7 @@ use Robo\Robo;
  */
 class DockworkerShellCommands extends DockworkerCommands
 {
-    use DeployedK8sResourcesTrait;
-    use DeployedLocalResourcesTrait;
-    use DockerCliTrait;
-    use GitHubClientTrait;
-    use KubectlCliTrait;
+    use DockerContainerExecTrait;
 
     /**
      * Opens a shell into the application.
@@ -46,91 +38,6 @@ class DockworkerShellCommands extends DockworkerCommands
             'Opening shell in %s. Type \'exit\' to close.',
             $options['env']
           )
-        );
-    }
-
-    /**
-     * Executes a command in the container.
-     */
-    protected function executeContainerCommand(
-      $env,
-      $command,
-      $io,
-      $title = '',
-      $message = '',
-      $init = true
-    ): void
-    {
-        if ($init) {
-            $this->initShellCommand($env);
-        }
-        $container = $this->getDeployedContainer(
-            $io,
-            $env
-        );
-        if (!empty($title)) {
-            $io->title($title);
-        }
-        if (!empty($message)) {
-            $io->say($message);
-        }
-        $container->run(
-          $command,
-          $io
-        );
-    }
-
-    protected function executeContainerCommandSet(
-        $env,
-        array $commands,
-        $io,
-        $title = '',
-    ): void
-    {
-        $first_command = true;
-        foreach ($commands as $command) {
-            if ($first_command) {
-                $title_string = $title;
-                $needs_init = true;
-                $first_command = false;
-            }
-            else {
-                $title_string = '';
-                $needs_init = false;
-            }
-            $this->executeContainerCommand(
-              $env,
-              $command['command'],
-              $io,
-              $title_string,
-              $command['message'],
-              $needs_init
-            );
-        }
-    }
-
-    /**
-     * Initializes the command and executes all preflight checks.
-     *
-     * @param string $env
-     *   The environment to initialize the command for.
-     */
-    protected function initShellCommand(string $env): void
-    {
-        if ($env === 'local') {
-            // $this->initGitHubClientApplicationRepo();
-            // $this->setStackTeamsClient('unblibsystems');
-            $this->registerDockerCliTool($this->dockworkerIO);
-            $this->enableLocalResourceDiscovery();
-        } else {
-            $this->registerKubectlCliTool($this->dockworkerIO);
-            $this->enableK8sResourceDiscovery();
-        }
-        $this->checkPreflightChecks($this->dockworkerIO);
-        $this->discoverDeployedResources(
-            $this->dockworkerIO,
-            Robo::config(),
-            $env
         );
     }
 
