ed3285a255e67f2bc11d7a6fefc58af66471afeb
Jacob Sanford <jsanford@unb.ca>
2023-03-24 16:32:36 -0300

    Monitor startup logs for finish marker and exit after deployment

--- src/Robo/Plugin/Commands/DaemonDeployCommands.php
+++ src/Robo/Plugin/Commands/DaemonDeployCommands.php
@@ -2,6 +2,7 @@
 
 namespace Dockworker\Robo\Plugin\Commands;
 
+use Dockworker\Cli\CliCommand;
 use Dockworker\Cli\DockerCliTrait;
 use Dockworker\Cli\KubectlCliTrait;
 use Dockworker\Core\CommandLauncherTrait;
@@ -50,6 +51,89 @@ class DaemonDeployCommands extends DockworkerDaemonCommands
         );
         $this->buildComposeApplication();
         $this->startComposeApplication();
-        $this->followComposeApplicationLogs();
+        $this->monitorLocalDeploymentProgress();
+        $this->say('Application deployment complete.');
+    }
+
+    /**
+     * Monitors the local deployment progress.
+     *
+     * @throws \Dockworker\DockworkerException
+     */
+    private function monitorLocalDeploymentProgress(): void
+    {
+        $this->dockworkerIO->section("[local] Application Deployment");
+        $cmd = $this->startLocalDeploymentLogFollowingCommand();
+        $error_found = false;
+        while (
+           !str_contains($cmd->getOutput(), '99_z_report_completion')
+        ) {
+            if ($this->outputHasErrors($cmd->getOutput())) {
+                $error_found = true;
+                break;
+            }
+            usleep(500);
+        }
+        if ($error_found) {
+            $cmd->signal(9);
+            $this->dockworkerIO->error('Application deploy failed.');
+            exit(1);
+        }
+        $cmd->stop();
+    }
+
+    /**
+     * Starts the local deployment log following command.
+     *
+     * @return \Dockworker\Cli\CliCommand
+     */
+    private function startLocalDeploymentLogFollowingCommand(): CliCommand {
+        $cmd = $this->getLocalDeploymentLogFollowingCommand();
+        $name = $this->applicationName;
+        $cmd->start(function ($type, $buffer) use ($name) {
+            // Stream is colorless here, so make it easier to read.
+            $colored_buffer = str_replace(
+                "$name  |",
+                "\033[34m$name  |\033[0m",
+                $buffer
+            );
+            echo $colored_buffer;
+        });
+        return $cmd;
+    }
+
+    /**
+     * Gets the local deployment log following command.
+     *
+     * @return \Dockworker\Cli\CliCommand
+     */
+    private function getLocalDeploymentLogFollowingCommand($timeout = 300): CliCommand {
+        return new CliCommand(
+            [
+                'docker',
+                'compose',
+                'logs',
+                '-f',
+                $this->applicationName,
+            ],
+            '',
+            $this->applicationRoot,
+            [],
+            null,
+            $timeout
+        );
+    }
+
+    /**
+     * Checks if the output has errors.
+     *
+     * @param string $output
+     *   The output to check.
+     *
+     * @return bool
+     *   TRUE if the output has errors, FALSE otherwise.
+     */
+    private function outputHasErrors(string $output): bool {
+        return str_contains($output, 'ERROR');
     }
 }
